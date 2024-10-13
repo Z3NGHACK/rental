ba8630eb02ceeff2a195dec26a72663f3e58292d
Z3NGHACK <148023899+Z3NGHACK@users.noreply.github.com>
2024-10-13 22:53:45 +0700

    Add files via upload

---
+++ rental-page/script.js
@@ -0,0 +1,6 @@
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn")
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "YOUR-OPENAI-API-KEY-HERE";
+let isImageGenerating = false;
